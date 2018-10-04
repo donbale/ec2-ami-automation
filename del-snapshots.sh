f796d9a7e7f08ec3f9f221712715bafb51f0cf15
Noble <32083297+tln8679@users.noreply.github.com>
2018-10-04 13:22:49 -0400

    user must enter their own acct #

--- del-snapshots.sh
+++ del-snapshots.sh
@@ -3,7 +3,7 @@
 # Date is 4 days ago, but will delete/deregister anything 5 days or older
 __PASTDUE=$(date --date="4 days ago" +"%Y-%m-%d")
 
-images_to_deregister=($(aws ec2 describe-images --owners 403057575040 --query 'Images[?CreationDate<`'"$__PASTDUE"'`].{ID:ImageId}'))
+images_to_deregister=($(aws ec2 describe-images --owners <Insert your account number> --query 'Images[?CreationDate<`'"$__PASTDUE"'`].{ID:ImageId}'))
 echo "List of images to deregister as of $__PASTDUE: $images_to_deregister"
 
 # Deregister images
@@ -12,7 +12,7 @@ for image in $images_to_deregister; do
   aws ec2 deregister-image --image-id $image
 done
 
-snapshots_to_delete=($(aws ec2 describe-snapshots --owner-ids 403057575040 --query 'Snapshots[?StartTime<`'"$__PASTDUE"'`].SnapshotId'))
+snapshots_to_delete=($(aws ec2 describe-snapshots --owner-ids <Insert your account number> --query 'Snapshots[?StartTime<`'"$__PASTDUE"'`].SnapshotId'))
 echo "List of snapshots to delete after $__PASTDUE: $snapshots_to_delete"
 
 # Delete snaphsots
